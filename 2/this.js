2e38ccd68ef31a0bdfa751070cb71a150de2c469
GyoHeonLee <ghl12078@gmail.com>
2022-01-16 09:29:23 +0900

    week2-this keyword

--- 2/this.js
+++ 2/this.js
@@ -1,19 +1,29 @@
 // !node js와 browser js의 this는 서로 다르게 정의됩니다.
-// 전역에서 사용되는 this는 전역(global)과 같다.
-var a = 27; // let, const는 안된다.
-console.log(this.a);
 
-// 함수의 this
+// 전역에서 사용되는 this는 전역(global)과 같다.
+console.log("전역에서 사용되는 this는 전역(global)과 같다.");
+var global = 27; // let, const는 안된다.
+console.log(this.global);
+
+// 함수의 this => 함수의 this는 window다.
+console.log("함수의 this => 함수의 this는 window다.");
 function sum() {
   console.log(this.a + this.b + this.c);
 }
-const obj = { a: 1, b: 2, c: 3, sum };
+var a = 1;
+var b = 2;
+var c = 3;
+sum();
 
+const obj = { a: 2, b: 3, c: 4, sum };
 obj.sum();
 
 // codejong
+console.log("코딩종");
 // !!!!!호출한 놈 === this!!!!! //
+
 // someone이 this임.
+console.log("someone이 this임.");
 var someone = {
   name: "LeeGyoheon",
   whoAmI: function () {
@@ -23,16 +33,20 @@ var someone = {
 someone.whoAmI(); //호출 => someone이 호출한 거임.
 
 // window가 this임.
+console.log("window가 this임.");
 var myWhoAmI = someone.whoAmI;
+console.log(myWhoAmI);
 myWhoAmI(); // 호출 => window가 호출한 거임.
 
 // btn이 this임
+console.log("btn이 this임");
 var btn = document.querySelector("#btn");
 btn.addEventListener("click", someone.whoAmI);
 btn.addEventListener("click", myWhoAmI);
 //btn에 whoAmI를 넘긴것 과 똑같음 => 호출한게 아니니까!(함수를 넘기기만 했을 뿐임.)
 
 // 객체안 객체?
+console.log("객체안 객체?");
 var objOut = {
   age: 27,
   sayOut: function () {
@@ -51,6 +65,8 @@ objOut.sayOut();
 objOut.objIn.sayIn();
 
 // function안 function?
+// 결국 함수 선언은 윈도우 객체안에 함수를 선언했다고 볼 수 있다.
+console.log("function안 function?");
 function funcOut() {
   console.log(this);
   function funcIn() {
@@ -70,8 +86,40 @@ var arrowObj = {
 
 arrowObj.arrowFunc();
 
-function test() {
-  console.log(this);
+// 생활 코딩
+console.log("생활코딩");
+
+console.log("함수의 this는 window다.");
+function func() {
+  if (this === window) {
+    console.log("window===this");
+  }
+}
+func();
+
+console.log("객체의 소속인 메소드의 this는 그 객체이다.");
+var o = {
+  func: function () {
+    if (o === this) {
+      console.log("o===this");
+    }
+  },
+};
+o.func();
+
+console.log("생성자의 호출");
+var funcThis = null;
+
+function Func() {
+  funcThis = this;
 }
 
-arrowObj.test;
+var o1 = Func();
+if (funcThis === window) {
+  console.log("window");
+}
+
+var o2 = new Func();
+if (funcThis === o2) {
+  console.log("o2");
+}
